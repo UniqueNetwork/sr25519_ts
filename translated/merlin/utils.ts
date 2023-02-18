7e83668e265e8a3fec77d8ad3f137fbc162be1cd
Alex Saft <fend25@gmail.com>
2023-02-18 15:11:44 +0300

    refactored b``

--- translated/merlin/utils.ts
+++ translated/merlin/utils.ts
@@ -1,9 +1,3 @@
 export const b = (tmpl: TemplateStringsArray, ...values: any[]): Uint8Array => {
-  const str = String.raw(tmpl, ...values)
-
-  const arr = new Uint8Array(str.length)
-  for (let i = 0; i < str.length; i++) {
-    arr[i] = str.charCodeAt(i)
-  }
-  return arr
+  return new TextEncoder().encode(String.raw(tmpl, ...values))
 }
