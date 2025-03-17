c68308f2a8f05ef26cad4fbcdfe9f3eec3050df8
Svg70 <collectionpiter@gmail.com>
2025-03-17 15:12:46 +0100

    upgrade signer types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,29 @@
+export interface SignerPayloadJSON {
+  address: string
+  blockHash: string
+  blockNumber: string
+  era: string
+  genesisHash: string
+  method: string
+  nonce: string
+  specVersion: string
+  tip: string
+  transactionVersion: string
+  signedExtensions: string[]
+  version: number
+}
+
+export interface SignerPayloadRaw {
+  data: string
+  address: string
+  type: 'bytes' | 'payload'
+}
+
 export type KeypairType = 'ed25519' | 'sr25519' | 'ecdsa' | 'ethereum'
 
 export interface UNIQUE_SDK_UnsignedTxPayloadBody {
-  signerPayloadJSON?: any
-  signerPayloadRaw?: any
+  signerPayloadJSON: SignerPayloadJSON
+  signerPayloadRaw: SignerPayloadRaw
   signerPayloadHex: string
 }
 
@@ -12,5 +33,6 @@ export interface UNIQUE_SDK_SignTxResultResponse {
 }
 
 export interface IUniqueSdkSigner {
+  address?: string
   sign: (unsignedTxPayload: UNIQUE_SDK_UnsignedTxPayloadBody) => Promise<UNIQUE_SDK_SignTxResultResponse>
 }
